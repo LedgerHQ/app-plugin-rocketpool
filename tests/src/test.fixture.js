4e418cd8bd1de2797816273a3e8b94bf0652d4bf
Adrien Lacombe <adrien.lacombe@ledger.fr>
2023-09-04 14:14:48 +0200

    update RP name (#2)
    * updtae RP name
    
    * rename icons
    
    * update RP name
    
    * fix, space not working here

--- tests/src/test.fixture.js
+++ tests/src/test.fixture.js
@@ -87,7 +87,7 @@ function zemu(device, func) {
         let elf_path;
         let lib_elf;
         elf_path = device.eth_path;
-        lib_elf = {'RocketPool': device.path};
+        lib_elf = {'Rocket\\ Pool': device.path};
 
         const sim = new Zemu(elf_path, lib_elf);
         try {
